94ae96f04aaa15e39459b2b35e66f87d830f60e8
Brett East <brett@sproutback.com>
2015-11-05 13:15:30 +1100

    Added a hireme page and updated some text

---
+++ builds/production/js/myApp.js
@@ -0,0 +1,30 @@
+(function(){
+	
+	angular.module("myApp", [])
+	
+	
+	.directive("mainNav", function() {
+		
+		return {
+			restrict: 'E',
+			templateUrl: 'main-nav.html',
+			replace: true
+		};
+		
+	})
+	
+	
+	.directive("mainFooter", function() {
+		
+		return {
+			restrict: 'E',
+			templateUrl: 'main-footer.html',
+			replace: true
+		};
+		
+	});
+	
+	
+	
+	
+})();
